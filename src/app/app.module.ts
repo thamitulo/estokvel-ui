24ec4fd77f29ec09fcecc73f2b61f5c8b7c54dda
Thami Tulo <thami.tulo@momentum.co.za>
2025-11-04 21:31:39 +0200

    upgrade of angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthConfig, AuthModule } from '@auth0/auth0-angular';
 import { AuthHttpInterceptor } from './auth.interceptor';
 import { environment } from './environments/environment';
@@ -12,27 +12,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './shared/footer/footer.component';
 import { FaqComponent } from './shared/faq/faq.component';
 
-@NgModule({
-  declarations: [AppComponent, ToolbarComponent, FooterComponent, FaqComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    AuthModule.forRoot({
-      domain: environment.auth0.domain,
-      clientId: environment.auth0.clientId,
-      authorizationParams: {
-        redirect_uri: environment.auth0.redirectUri,
-        audience: environment.auth0.audience,
-    },
-
-    } as unknown as AuthConfig),
-    MaterialModule,
-    BrowserAnimationsModule
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true }
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [AppComponent, ToolbarComponent, FooterComponent, FaqComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        AuthModule.forRoot({
+            domain: environment.auth0.domain,
+            clientId: environment.auth0.clientId,
+            authorizationParams: {
+                redirect_uri: environment.auth0.redirectUri,
+                audience: environment.auth0.audience,
+            },
+        } as unknown as AuthConfig),
+        MaterialModule,
+        BrowserAnimationsModule], providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: AuthHttpInterceptor, multi: true },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
